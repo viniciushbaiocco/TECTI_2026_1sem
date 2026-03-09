37eb6d865e75695530d3de59ca04b6863fff9c6d
Vinicius Hegues Baiocco <vinicius.hb2@puccampinas.edu.br>
2026-03-09 10:25:48 -0300

    testando como vscode funciona em relação ao git

--- gojo.py
+++ gojo.py
@@ -1,1 +1,8 @@
-print ("gojo  vive desgraça")
+num = float(input("bota um numero ai: "))
+if num <= 10:
+    if num == 10:
+        print("tirou 10")
+    else:
+        print ("nao tirou 10 mas passou")
+else:
+    print("o numero é maior que 10 ")
